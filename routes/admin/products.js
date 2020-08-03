8208c1c0d2dc01f52b5a2b444eefae42d2b0c3b5
wabomba-bakar <wabombabakar@gmail.com>
2020-08-03 17:55:10 +0300

    cleaned up file upload filters

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -3,9 +3,11 @@ const auth = require("../../middleware/auth");
 const {
   postAddProduct,
   postAdminEditProduct,
+  postAdminDeleteProduct,
 } = require("../../controllers/admin/products");
 
 router.route("/add-product").post(auth, postAddProduct);
 router.route("/edit-product").post(auth, postAdminEditProduct);
+router.route("/delete-product").post(auth, postAdminDeleteProduct);
 
 module.exports = router;
--- utils/file.js
+++ utils/file.js
@@ -1,24 +1,26 @@
 const { videoPath, audioPath, imagePath } = require("./file-paths");
 const fs = require("fs");
 
-exports.deleteFile = function (filePath) {
-  return fs.unlink(filePath, function (ex) {
+exports.deleteFile = (filePath) =>
+  fs.unlink(filePath, (ex) => {
     if (ex) throw ex;
   });
-};
 
 exports.fileFilter = (res, file) => {
   const fileType = file.mimetype.split("/")[0];
-
-  const videoMimes = () => {
-    const mimes = ["video/mp4", "video/webm", "video/x-matroska", "video/mpeg"];
-    const fileformat = "Video";
+  const filterMimes = (mimes, fileformat) => {
     const [validMimetype] = mimes.filter((mime) => mime === file.mimetype);
     if (!validMimetype)
       return res.status(400).json({
         success: false,
         msg: `Unsupported file, expected to upload ${fileformat} file but got ${fileType} file.`,
       });
+  };
+
+  const videoMimes = () => {
+    const mimes = ["video/mp4", "video/webm", "video/x-matroska", "video/mpeg"];
+    const fileformat = "Video";
+    filterMimes(mimes, fileformat);
     const videoSize = 1024 * 1024 * 1024 * 2;
     if (file.size > videoSize)
       return res.status(400).json({
@@ -28,15 +30,10 @@ exports.fileFilter = (res, file) => {
     return videoPath(file);
   };
 
-  const audioMimes = function () {
+  const audioMimes = () => {
     const mimes = ["audio/mp3", "audio/mpeg"];
     const fileformat = "Audio";
-    const [validMimetype] = mimes.filter((mime) => mime === file.mimetype);
-    if (!validMimetype)
-      return res.status(400).json({
-        success: false,
-        msg: `Unsupported file, expected to upload ${fileformat} file but got ${fileType} file.`,
-      });
+    filterMimes(mimes, fileformat);
     const audioSize = 1024 * 1024 * 100;
     if (file.size > audioSize)
       return res.status(400).json({
@@ -49,12 +46,7 @@ exports.fileFilter = (res, file) => {
   const imageMimes = () => {
     const mimes = ["image/jpg", "image/png", "image/gif", "image/jpeg"];
     const fileformat = "Image";
-    const [validMimetype] = mimes.filter((mime) => mime === file.mimetype);
-    if (!validMimetype)
-      return res.status(400).json({
-        success: false,
-        msg: `Unsupported file, expected to upload ${fileformat} file but got ${fileType} file.`,
-      });
+    filterMimes(mimes, fileformat);
     const imageSize = 1024 * 1024 * 5;
     if (file.size > imageSize)
       return res
