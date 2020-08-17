4b7653307c0c6d87ce93771d1ed52f3d9955a421
wabomba-bakar <wabombabakar@gmail.com>
2020-08-17 15:03:46 +0300

    added remote mongodb database.

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,6 @@
-if (process.env.NODE_ENV === 'production') return module.exports = { mongoURI: '' };
-return module.exports = { mongoURI: 'mongodb://localhost:27017/rest-api' };
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    mongoURI:
+      "mongodb+srv://casper:h0L4Vc7Hw1LJ1N0P@ecom-app-qkg4p.mongodb.net/Ecom-app?retryWrites=true&w=majority",
+  };
+} else module.exports = { mongoURI: "mongodb://localhost:27017/rest-api" };
