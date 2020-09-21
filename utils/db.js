7bf82dea20657fc52b46b97c6ad35283226227b8
bakar-coder <freelancebakar@gmail.com>
2020-09-21 13:30:52 +0300

    cleanedup

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,6 @@ const config = require('config')
 
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI: 'mongodb+srv://ghetto:yw7SMS8DpEXaZL56@ghettohustler.bqogt.mongodb.net/ghetto?retryWrites=true&w=majority'
+    mongoURI: config.get('proddb')
   };
 } else module.exports = { mongoURI: "mongodb://localhost:27017/rest-api" };
