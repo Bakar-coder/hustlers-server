a8590e6608e677d1540f19ed51e562e80167f78d
bakar-coder <freelancebakar@gmail.com>
2020-09-21 12:36:36 +0300

    cleanedup

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,8 @@
+const config = require('config')
+
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      process.env.mongoURI,
+      config.get('proddb'),
   };
 } else module.exports = { mongoURI: "mongodb://localhost:27017/rest-api" };
