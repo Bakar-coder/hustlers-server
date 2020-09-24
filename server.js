2cdd3a75fd8a8d1bef0c2912af65456dcd7c0aed
Wabomba Bakar <56803927+Bakar-coder@users.noreply.github.com>
2020-09-24 10:55:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,14 @@ const helmet = require("helmet");
 const port = process.env.PORT || 5000;
 const app = express();
 
+const corsMiddleware  = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'localhost'); //replace localhost with actual host
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
+
+    next();
+}
+
 if (!config.get("jwtPrivateKey")) {
   console.error("************ No private provided *************");
   process.exit(1);
@@ -50,6 +58,7 @@ app.use("/media", express.static(join(__dirname, "media")));
 app.use("/media/videos", express.static(join(__dirname, "media/videos")));
 app.use("/media/photos", express.static(join(__dirname, "media/photos")));
 app.use("/media/audios", express.static(join(__dirname, "media/audios")));
+app.use(corsMiddleware);
 app.use(cors());
 app.use(
   morgan("combined", {
